1b3b80c4fcba3f76969a084273d709e26f80de1f
Yolijn <yolijn@frameless.io>
2022-01-05 18:36:23 +0100

    ci: update dependabot and ncurc configurations to match other nlds repos

--- .ncurc.patch.js
+++ .ncurc.patch.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reject: ['@angular-devkit/build-angular'],
+  reject: [
+    // Angular needs to be upgraded in combination with Typescript.
+    // Somehow devkit is a patch version which matches the 11 major Angular package
+    '@angular-devkit/build-angular',
+  ],
 };
