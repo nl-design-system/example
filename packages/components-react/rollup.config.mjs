b12362343fea09c35f11fb76b9de3b31e369b7e6
AndreaBusse29 <andrea.busse@the-experts.nl>
2023-07-24 17:29:25 +0200

    chore: add components-react package

---
+++ packages/components-react/rollup.config.mjs
@@ -0,0 +1,62 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import { readFileSync } from "fs";
+import filesize from "rollup-plugin-filesize";
+import nodeExternal from "rollup-plugin-node-externals";
+import nodePolyfills from "rollup-plugin-node-polyfills";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "rollup-plugin-typescript2";
+
+const packageJson = JSON.parse(readFileSync("./package.json", "utf8"));
+
+// rollup.config.js
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+
+export const outputGlobals = {
+  react: "React",
+  "react-dom": "ReactDOM",
+};
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        globals: outputGlobals,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+        globals: outputGlobals,
+      },
+    ],
+    external: [/@babel\/runtime/, "react-dom", "react"],
+    plugins: [
+      peerDepsExternal({ includeDependencies: true }),
+      nodeExternal(),
+      resolve({ browser: true }),
+      commonjs({
+        include: /node_modules/,
+      }),
+      nodePolyfills(),
+      // del({ targets: ['dist/*', 'pages/*'] }),
+      typescript({ includeDependencies: false }),
+      babel({
+        presets: ["@babel/preset-react"],
+        babelHelpers: "runtime",
+        exclude: ["node_modules/**", "dist/**"],
+        extensions: [".ts", ".tsx"],
+        inputSourceMap: true,
+        plugins: ["@babel/plugin-transform-runtime"],
+      }),
+      filesize(),
+    ],
+  },
+];
---
+++ packages/components-react/vite.config.ts
@@ -0,0 +1,29 @@
+/* eslint-env node */
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Components React',
+      // the proper extensions will be added
+      fileName: 'components-react',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
