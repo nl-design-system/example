d7009bc114ea7720dc7e916e4881a85f6d2687ef
AndreaBusse29 <andrea.busse@the-experts.nl>
2023-07-25 15:06:38 +0200

    feat: add sparkle effect to button

--- packages/components-react/src/Button.tsx
+++ packages/components-react/src/Button.tsx
@@ -1,10 +1,18 @@
 import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
+import Sparkle from 'react-sparkle';
 
 export const Button = forwardRef(
   ({ children, className, ...restProps }: PropsWithChildren<any>, ref: ForwardedRef<HTMLButtonElement>) => {
     return (
-      <button ref={ref} className={className} type={'button'} {...restProps}>
+      <button ref={ref} className={className} type={'button'} {...restProps} style={{ position: 'relative' }}>
         {children}
+        <Sparkle
+          overflowPx={100}
+          fadeOutSpeed={100}
+          flicker={true}
+          flickerSpeed={'slowest'}
+          color={['#FFF', '#8a0000', '#000000', '#0db5b8', '#f71e2c']}
+        />
       </button>
     );
   },
