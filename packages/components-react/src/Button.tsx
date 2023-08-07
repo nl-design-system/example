f06a1287b8c90388a2ebb298fc386eb85ac497b9
Robbert Broersma <git@robbertbroersma.nl>
2023-08-07 18:52:28 +0200

    refactor: build button component more like existing components

--- packages/components-react/src/Button.tsx
+++ packages/components-react/src/Button.tsx
@@ -1,9 +1,13 @@
-import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import { ButtonHTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren } from 'react';
+import '@example/components-css/button/index.scss';
+
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = forwardRef(
-  ({ children, className, ...restProps }: PropsWithChildren<any>, ref: ForwardedRef<HTMLButtonElement>) => {
+  ({ children, className, ...restProps }: PropsWithChildren<ButtonProps>, ref: ForwardedRef<HTMLButtonElement>) => {
     return (
-      <button ref={ref} className={className} type={'button'} {...restProps}>
+      <button ref={ref} className={clsx('example-button', className)} {...restProps}>
         {children}
       </button>
     );
