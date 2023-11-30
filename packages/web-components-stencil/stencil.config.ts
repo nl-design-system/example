ae8dd8379eb849b3ceb416d2b7c6c0b3445b3c46
Robbert Broersma <git@robbertbroersma.nl>
2023-11-30 15:15:09 +0100

    build: major upgrade to latest Stencil

--- packages/web-components-stencil/stencil.config.ts
+++ packages/web-components-stencil/stencil.config.ts
@@ -14,10 +14,15 @@ export const config: Config = {
       type: 'dist-custom-elements',
       customElementsExportBehavior: 'bundle',
     },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
     reactOutputTarget({
       componentCorePackage: '@example/web-components-stencil',
       proxiesFile: '../web-components-react/src/components.ts',
     }),
   ],
   plugins: [sass()],
+  extras: { enableImportInjection: true },
 };
