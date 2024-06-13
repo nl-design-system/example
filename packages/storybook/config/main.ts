a3165b7b6ed55a081f78ca32525f42d9c3a8ee8e
matijs <matijs@gmail.com>
2024-06-13 11:27:40 +0100

    build: patch round 2024-06
    - install patch updates
    - install minor updates
    - install major updates
      - prevent eslint-plugin-json from updating because it requires ESLint
        v9
    
    - Re-add `@whitespace/storybook-addon-html` and
    `@etchteam/storybook-addon-status` now that they fully support Storybook
    v8

--- packages/storybook/config/main.ts
+++ packages/storybook/config/main.ts
@@ -2,7 +2,13 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*stories.@(js|jsx|ts|tsx)', '../src/**/*.mdx'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-viewport'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport',
+    '@whitespace/storybook-addon-html',
+    '@etchteam/storybook-addon-status',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
