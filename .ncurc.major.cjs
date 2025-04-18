26cd4a719d0eac7463481813d48151424a4afab0
matijs <matijs@gmail.com>
2025-04-18 11:56:58 +0200

    fix: npm-check-updates major config
    Remove eslint and eslint-plugin-json from the npm-check-updates config
    so they're updated once more

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -8,8 +8,6 @@ module.exports = {
     '@types/node',
     '@types/react',
     '@types/react-dom',
-    'eslint',
-    'eslint-plugin-json',
     'react',
     'react-dom',
     'style-dictionary',
