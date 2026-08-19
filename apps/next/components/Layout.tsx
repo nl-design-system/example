50faf39eb19e78ef0fa6571b8eb5da4f61e29049
Eelco Bosklopper <eelcobosklopper@gmail.com>
2026-08-19 10:19:09 +0200

    feat: move navItems outside of Navbar

--- apps/next/components/Layout.tsx
+++ apps/next/components/Layout.tsx
@@ -1,20 +1,33 @@
 import { Root } from '@utrecht/root-react';
 import { ReactNode } from 'react';
 import { Logo } from '../components/Logo';
-import { Navbar, type NavbarProps } from '../components/Navbar/Navbar';
+import { Navbar } from '../components/Navbar/Navbar';
 import { PageBody } from '../components/PageBody/PageBody';
 import { PageFooter } from '../components/PageFooter/PageFooter';
 import { PageHeader } from '../components/PageHeader/PageHeader';
 import { SkipLink } from '../components/SkipLink/SkipLink';
 import '@utrecht/root-css/dist/index.css';
 
+type NavbarCurrent = 'detail' | 'form' | 'home';
+
 type LayoutProps = {
   theme: 'start-theme' | 'rhc-theme';
   children: ReactNode;
-  navbarCurrent: NavbarProps['current'];
+  navbarCurrent: NavbarCurrent;
 };
 
+const navPages: { id: NavbarCurrent; href: string; label: string }[] = [
+  { id: 'home', href: '/', label: 'Home' },
+  { id: 'detail', href: '/detail', label: 'Detail page' },
+  { id: 'form', href: '/form', label: 'Form' },
+];
+
 const Layout = ({ children, navbarCurrent, theme }: LayoutProps) => {
+  const navItems = navPages.map((page) => ({
+    ...page,
+    'aria-current': (page.id === navbarCurrent ? 'page' : undefined) as 'page' | undefined,
+  }));
+
   return (
     <Root lang="en" dir="ltr" className={theme}>
       <body>
@@ -24,7 +37,7 @@ const Layout = ({ children, navbarCurrent, theme }: LayoutProps) => {
           <Logo />
         </PageHeader>
 
-        <Navbar current={navbarCurrent} />
+        <Navbar items={navItems} />
 
         <PageBody id="main">{children}</PageBody>
 
--- apps/next/components/Navbar/Navbar.tsx
+++ apps/next/components/Navbar/Navbar.tsx
@@ -1,32 +1,16 @@
 import { NavBar } from '@rijkshuisstijl-community/navigation-bar-react';
 
-export interface NavbarProps {
-  current: 'detail' | 'form' | 'home';
+export interface NavbarItem {
+  id: string;
+  'aria-current': 'page' | undefined;
+  href: string;
+  label: string;
 }
 
-const Navbar = ({ current }: NavbarProps) => (
-  <NavBar
-    items={[
-      {
-        id: 'home',
-        'aria-current': current === 'home' ? 'page' : undefined,
-        href: '/',
-        label: 'Home',
-      },
-      {
-        id: 'detail',
-        'aria-current': current === 'detail' ? 'page' : undefined,
-        href: '/detail',
-        label: 'Detail page',
-      },
-      {
-        id: 'form',
-        'aria-current': current === 'form' ? 'page' : undefined,
-        href: '/form',
-        label: 'Form',
-      },
-    ]}
-  />
-);
+export interface NavbarProps {
+  items: NavbarItem[];
+}
+
+const Navbar = ({ items }: NavbarProps) => <NavBar items={items} />;
 
 export { Navbar };
